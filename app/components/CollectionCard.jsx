632bf09e29cdfd79c2fa04032d6c78ff1a3116c4
Adam Chipperfield <achipperfield@corra.com>
2023-04-25 10:51:03 +0100

    feat: added aspect ratio to the collection card images

--- app/components/CollectionCard.jsx
+++ app/components/CollectionCard.jsx
@@ -4,10 +4,16 @@ import { Image } from '@shopify/hydrogen'
 export default function CollectionCard({ collection }) {
   return (
     <Link to={`/collections/${collection.handle}`}>
-      <Image
-        data={collection.image}
-        alt={collection.title}
-      />
+      <div className="bg-slate-200 aspect-[1]">
+        {collection.image && (
+          <Image
+            data={collection.image}
+            alt={collection.title}
+            aspectRatio="1"
+            className="object-cover"
+          />
+        )}
+      </div>
 
       <h3 className="text-sm mt-4">{collection.title}</h3>
     </Link>
